ae058a69f7ece005d3e1a7d8a98aa10c4c8f2753
Luce Liu <thisisluce@gmail.com>
2025-03-15 12:32:56 -0400

    add pagination

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Person } from "../types/Person";
 
 interface ITableProps {
@@ -9,6 +10,26 @@ const Table: React.FunctionComponent<ITableProps> = ({
   data,
   itemsPerPage = 50,
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const getCurrentPageData = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    return data.slice(startIndex, endIndex);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prev) => Math.min(prev + 1, totalPages));
+  };
+
+  const currentData = getCurrentPageData();
+
   return (
     <div>
       <table>
@@ -22,7 +43,7 @@ const Table: React.FunctionComponent<ITableProps> = ({
           </tr>
         </thead>
         <tbody>
-          {data.map((person) => (
+          {currentData.map((person) => (
             <tr key={person.id}>
               <td>{person.name}</td>
               <td>{person.dob}</td>
@@ -36,6 +57,33 @@ const Table: React.FunctionComponent<ITableProps> = ({
           ))}
         </tbody>
       </table>
+
+      {/* footer */}
+      <div className="flex justify-between items-center">
+        <div>
+          <span>
+            Showing <span>{(currentPage - 1) * itemsPerPage + 1}</span> to{" "}
+            <span>{Math.min(currentPage * itemsPerPage, data.length)}</span> of{" "}
+            <span>{data.length}</span> entries
+          </span>
+        </div>
+        <div>
+          <button
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+            className="border"
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+            className="border"
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
