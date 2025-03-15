dfaa4b52187b50e612582e9c8bf413a444879892
Luce Liu <thisisluce@gmail.com>
2025-03-15 12:05:06 -0400

    start project

---
+++ src/components/Table.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ITableProps {}
+
+const Table: React.FunctionComponent<ITableProps> = (props) => {
+  return <div>Table here</div>;
+};
+
+export default Table;
